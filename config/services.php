60c8ed8358678ddeba634320dd14b6f5b651110d
root <root@fiscaldock.com.br>
2025-12-29 03:54:30 +0000

    fix: EFD Fiscal Completo funcionando (v1.0.2)

--- config/services.php
+++ config/services.php
@@ -41,7 +41,7 @@ return [
         'sped_fiscal_url' => env('WEBHOOK_SPED_FISCAL_URL', 'https://autowebhook.fiscaldock.com.br/webhook/raf-consulta-gratuita-sped-fiscal'),
         // Webhooks para modalidade "completa" (CND + regime tributário)
         'sped_contribuicoes_completa_url' => env('WEBHOOK_SPED_CONTRIBUICOES_COMPLETA_URL', 'https://autowebhook.fiscaldock.com.br/webhook/raf-consulta-completa-sped-contribuicoes'),
-        'sped_fiscal_completa_url' => env('WEBHOOK_SPED_FISCAL_COMPLETA_URL', 'https://autowebhook.fiscaldock.com.br/webhook/consultar-cnd-e-regime-tributario-sped-fiscal'),
+        'sped_fiscal_completa_url' => env('WEBHOOK_SPED_FISCAL_COMPLETA_URL', 'https://autowebhook.fiscaldock.com.br/webhook/raf-consulta-completa-sped-fiscal'),
         'username' => env('WEBHOOK_SPED_USERNAME'),
         'password' => env('WEBHOOK_SPED_PASSWORD'),
     ],
